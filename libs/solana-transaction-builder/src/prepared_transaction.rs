82ea0e39748e9c9d8483eb02175fecfe1dc88185
Ondra Chaloupka <chalda@marinade.finance>
2024-05-07 14:08:22 +0200

    Possible to add description to instruction builder instructions

--- libs/solana-transaction-builder/src/prepared_transaction.rs
+++ libs/solana-transaction-builder/src/prepared_transaction.rs
@@ -21,6 +21,7 @@ pub trait SignedTransaction {
 pub struct PreparedTransaction {
     pub transaction: Transaction,
     pub signers: Vec<Rc<Keypair>>,
+    pub instruction_descriptions: Vec<Option<String>>,
 }
 
 impl SignedTransaction for PreparedTransaction {
@@ -42,11 +43,36 @@ impl PreparedTransaction {
     pub fn new(
         transaction: Transaction,
         signature_builder: &SignatureBuilder,
+        instruction_descriptions: Vec<Option<String>>,
     ) -> Result<Self, Pubkey> {
         let signers = signature_builder.signers_for_transaction(&transaction)?;
         Ok(Self {
             transaction,
             signers,
+            instruction_descriptions,
         })
+    }
+
+    pub fn single_description(&self) -> Option<String> {
+        let mut descriptions = self
+            .instruction_descriptions
+            .iter()
+            .map(|desc| desc.clone())
+            .collect::<Vec<_>>();
+        for (i, description) in descriptions.iter_mut().enumerate() {
+            if let Some(desc) = description {
+                *description = Some(format!("#{}: {}", i, desc));
+            }
+        }
+        let descriptions = descriptions
+            .into_iter()
+            .filter_map(|d| d.map_or_else(|| None, |d| Some(d)))
+            .collect::<Vec<String>>()
+            .join("\n");
+        if descriptions.is_empty() {
+            None
+        } else {
+            Some(descriptions)
+        }
     }
 }
